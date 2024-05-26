5b27b2e52a61fa0890967774046f613fca2b18eb
shahrier <102902752+shahrierNafis@users.noreply.github.com>
2024-05-26 20:38:10 +0600

    2nd commit

--- src/script.js
+++ src/script.js
@@ -1,11 +1,13 @@
-await customElements.whenDefined("math-field");
-const mf = document.getElementById("formula");
-let latex = (localStorage.getItem("latex"))
-if (!latex || latex == "") {
-    latex = 'x=\\frac{-b\\pm \\sqrt{b^2-4ac}}{2a}'
-}
-mf.setValue(latex)
-function onMathfieldInput() {
-    localStorage.setItem("latex", mf.value);
-}
-mf.addEventListener("input", onMathfieldInput);
+customElements.whenDefined("math-field").then(() => {
+    const mf = document.getElementById("formula");
+    let latex = (localStorage.getItem("latex"))
+    if (!latex || latex == "") {
+        latex = 'x=\\frac{-b\\pm \\sqrt{b^2-4ac}}{2a}'
+    }
+    mf.setValue(latex)
+    function onMathfieldInput() {
+        localStorage.setItem("latex", mf.value);
+    }
+    mf.addEventListener("input", onMathfieldInput);
+})
+
